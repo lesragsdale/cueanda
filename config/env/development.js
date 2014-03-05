b7e90083d48cba6796e0f1ebfb715ff5a4090237
Les <lesragsdale@gmail.com>
2014-03-05 02:42:02 -0500

    4got the heroku db config smh..

--- config/env/development.js
+++ config/env/development.js
@@ -2,8 +2,8 @@
 //db: "mongodb://localhost/mean-dev",
 //no port attribute
 module.exports = {
-    //db: "mongodb://cueanda:cueAnda2014@troup.mongohq.com:10014/app22580832",
-    db: "mongodb://localhost/mean-dev",
+    db: "mongodb://cueanda:cueAnda2014@troup.mongohq.com:10014/app22580832",
+    //db: "mongodb://localhost/mean-dev",
     app: {
         name: "MEAN - A Modern Stack - Development"
     },
